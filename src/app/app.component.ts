3864aecb3d6502de9754d87966629972e1b2b83b
Borja Ruiz <borja.ruiz@asg.com>
2018-08-28 21:54:57 +0200

    some stupid code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+
+  constructor() {
+    this.doSomethingA();
+    this.doSomethingB();
+  }
+
+  doSomethingA(): void {
+    for (let i = 0; i < 100; i++) {
+      if (i < 20) {
+        if (i < 10) {
+          if (i < 5) {
+            if (i < 4) {
+              if (i < 3) {
+                if (i < 1) {
+                  console.log("Wow!!!");
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  doSomethingB(): void {
+    for (let i = 0; i < 100; i++) {
+      if (i < 20) {
+        if (i < 10) {
+          if (i < 5) {
+            if (i < 4) {
+              if (i < 3) {
+                if (i < 1) {
+                  console.log("Wow!!!");
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
